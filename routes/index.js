5ee7bae4dc0f1f4fcfa756f6e46d88b4c0458caf
Syed-Faizan-1 <faaiz290302@gmail.com>
2024-04-25 10:32:01 +0500

    Add Login & Logout route & functionalities using passport

--- routes/index.js
+++ routes/index.js
@@ -5,30 +5,44 @@ const passport = require('passport');
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index');
 });
 
-router.get('/login', function(req, res, next) {
+router.get('/login', function (req, res, next) {
   res.render('login');
 });
 
-router.get('/register', function(req, res, next) {
+router.get('/register', function (req, res, next) {
   res.render('register');
 });
 
-router.post('/register', function(req, res, next) {
+router.post('/register', function (req, res, next) {
   const data = new userModel({
     username: req.body.username,
     email: req.body.email,
     contact: req.body.contact,
   })
   userModel.register(data, req.body.password)
-  .then(function(){
-    passport.authenticate("local")(req, res, function(){
-      res.redirect("/profile");
+    .then(function () {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/profile");
+      })
     })
-  })
 });
+
+router.post('/login', passport.authenticate("local", {
+  failureRedirect: "/",
+  successRedirect: "/profile"
+}), function (req, res, next) {
+
+});
+
+router.get("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  })
+})
 
 module.exports = router;
