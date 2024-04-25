751d273a130aaecc77ca1221fabd4e0b98def5bf
Syed-Faizan-1 <faaiz290302@gmail.com>
2024-04-25 17:26:00 +0500

    Add Navbar

--- routes/index.js
+++ routes/index.js
@@ -9,20 +9,20 @@ passport.use(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index');
+  res.render('index', {nav:true});
 });
 
 router.get('/login', function (req, res, next) {
-  res.render('login');
+  res.render('login', {nav:false});
 });
 
 router.get('/register', function (req, res, next) {
-  res.render('register');
+  res.render('register', {nav:false});
 });
 
 router.get('/profile', isLoggedIn, async function (req, res, next) {
   const user = await userModel.findOne({username: req.session.passport.user});
-  res.render('profile', {user});
+  res.render('profile', {user, nav: true});
 });
 
 router.post('/fileupload', isLoggedIn, upload.single("image"), async function (req, res, next) {
